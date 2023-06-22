25586e3a475ea28cc82c462dc9924690353a842c
Justin Hold <36420615+jhold1982@users.noreply.github.com>
2023-06-22 12:16:46 -0500

    Initial Commit

---
+++ PinchApp/PinchAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  PinchAppApp.swift
+//  PinchApp
+//
+//  Created by Justin Hold on 6/22/23.
+//
+
+import SwiftUI
+
+@main
+struct PinchAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
